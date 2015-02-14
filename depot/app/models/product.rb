1213404ceb7dfe06bff10700857a0d5a4cff9b1b
Quang <quang.vo31@gmail.com>
2015-02-14 06:25:41 -0800

    7.1 Validation

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+#validate that the title, description and image_url are not blank.
+validates :title, :description, :image_url,  presence: true
+#validate that each price is greater than 1 cent.
+validates :price, numericality: {greater_than_or_equal_to: 0.01}
+#validate that each title is unique
+validates :title, uniqueness: true
+#validate that the image format is either .gif, .jpg or .png
+#used allow_blank to avoid getting multiple errors if image_url field is blank.
+validates :image_url, allow_blank: true, format: {
+	with: %r{\.(gif|jpg|png)\Z}i, 
+	message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
